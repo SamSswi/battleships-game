46dbb7fac10bd5bbab5ac660e97c9c65da0602b7
SamSswi <sadovoisamuel7@gmail.com>
2022-10-29 00:08:21 +0000

    Add welcome message function

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_message(board_size, number_of_ships):
+    """
+    Displays the welcome message.
+    Specifies the board size and the number of ships.
+    Displays the coordinates for the top left corner.
+    """
+    # new_line = '\n'
+    message = f'''
+    ---------------------------------------------
+           Welcome to the BATTLESHIPS GAME!
+       Board Size: {board_size}; Number of Battleships: {number_of_ships}
+    Top left corner coordinates - row: 1, col: 1
+    ---------------------------------------------
+    
+    '''
+    print(message)
+    return message
+
+
+
+
+
+def main():
+    """
+    Main function
+    """
+    board_size = 5
+    number_of_ships = 4
+    welcome_message(board_size, number_of_ships)
+
+
+main()
